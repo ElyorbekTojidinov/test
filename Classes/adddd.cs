8b6ef12cd2bed4c6ce79656aeedd4ec5eb47be0f
ElyorbekTojidinov <122852262+ElyorbekTojidinov@users.noreply.github.com>
2023-01-17 16:02:27 +0500

    first commit

---
+++ Classes/adddd.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_6.Task_1.Classes
+{
+    internal class adddd
+    {
+    }
+}
